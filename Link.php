8cedc1628e8e934f108fd1b46cd02996bed3575a
wipas-p <wipas.p@gmail.com>
2019-07-22 14:22:23 +0700

    Update Link.php

--- Link.php
+++ Link.php
@@ -2,3 +2,6 @@ Link
 ==============
 https://www.katacoda.com/
 https://crypto-market-price.herokuapp.com/
+https://blog.mimacom.com/ocp-persistent-storage/
+https://developers.redhat.com/blog/2017/10/24/vsphere-cloud-provider-vmdk-dynamic-provisioning/
+https://www.tutorialspoint.com/openshift/openshift_administration
